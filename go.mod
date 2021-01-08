d56b8e0d42e06ef626b142d531bd7adb129fb48b
Cai Gwatkin <caigwatkin@gmail.com>
2021-01-08 13:26:21 +1300

    add schema validation

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-chi/cors v1.1.1
 	github.com/google/uuid v1.1.2
 	github.com/pkg/errors v0.9.1
+	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/sys v0.0.0-20201214210602-f9fddec55a1e // indirect
 	golang.org/x/tools v0.0.0-20201215192005-fa10ef0b8743 // indirect
 	google.golang.org/api v0.36.0
---
+++ schema/schema.go
@@ -0,0 +1,119 @@
+package schema
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	go_errors "github.com/caigwatkin/go/errors"
+	go_log "github.com/caigwatkin/go/log"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+type Client interface {
+	Validate(ctx context.Context, schemaFileName string, bytes []byte) error
+}
+
+func NewClient(ctx context.Context, logClient go_log.Client, schemaFileNames []string) (Client, error) {
+	logClient.Info(ctx, "Initializing", go_log.FmtStrings(schemaFileNames, "schemaFileNames"))
+
+	type schemaAndFileNameAndError struct {
+		Err      error
+		FileName string
+		Schema   *gojsonschema.Schema
+	}
+	ch := make(chan schemaAndFileNameAndError, len(schemaFileNames))
+	for _, schemaFileName := range schemaFileNames {
+		go func(schemaFileName string) {
+			logClient.Info(ctx, "Loading", go_log.FmtString("schemaFileName", schemaFileName))
+			schema, err := loadSchemaFromFile(schemaFileName)
+			if err != nil {
+				ch <- schemaAndFileNameAndError{
+					Err: go_errors.Wrapf(err, "Failed to load schema %q from file", schemaFileName),
+				}
+				return
+			}
+			ch <- schemaAndFileNameAndError{
+				FileName: schemaFileName,
+				Schema:   schema,
+			}
+			logClient.Info(ctx, "Loaded", go_log.FmtString("schemaFileName", schemaFileName))
+		}(schemaFileName)
+	}
+
+	schemaByFileName := make(map[string]*gojsonschema.Schema)
+
+	var err error
+	for range schemaFileNames {
+		schemaAndFileNameAndError := <-ch
+		if schemaAndFileNameAndError.Err != nil {
+			logClient.Error(ctx, "Failed to load schema from file in goroutine, will check others and return an error", go_log.FmtError(schemaAndFileNameAndError.Err))
+			err = go_errors.Wrap(schemaAndFileNameAndError.Err, "Failed to load schema from file in goroutine")
+		}
+		schemaByFileName[schemaAndFileNameAndError.FileName] = schemaAndFileNameAndError.Schema
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	logClient.Info(ctx, "Initialized")
+	return &client{
+		logClient:        logClient,
+		schemaByFileName: schemaByFileName,
+	}, nil
+}
+
+type client struct {
+	schemaByFileName map[string]*gojsonschema.Schema
+	logClient        go_log.Client
+}
+
+func loadSchemaFromFile(fileName string) (*gojsonschema.Schema, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return nil, go_errors.Wrap(err, "Failed to get directory")
+	}
+
+	schema, err := gojsonschema.NewSchema(gojsonschema.NewReferenceLoader(fmt.Sprintf("file://%s/%s", dir, fileName)))
+	if err != nil {
+		return nil, go_errors.Wrapf(err, "Failed to initialize schema for file %q", fileName)
+	}
+
+	return schema, nil
+}
+
+func (c client) Validate(ctx context.Context, schemaFileName string, bytes []byte) error {
+	c.logClient.Info(ctx, "Validating", go_log.FmtString(schemaFileName, "schemaFileName"), go_log.FmtInt(len(bytes), "len(bytes)"))
+
+	if len(bytes) == 0 {
+		return go_errors.NewStatus(http.StatusBadRequest, "BODY_MUST_EXIST")
+	}
+
+	schema, ok := c.schemaByFileName[schemaFileName]
+	if !ok {
+		return go_errors.Errorf("Schema %q not loaded", schemaFileName)
+	}
+
+	result, err := schema.Validate(gojsonschema.NewBytesLoader(bytes))
+	if err != nil {
+		return go_errors.NewStatus(http.StatusBadRequest, err.Error())
+	}
+
+	if !result.Valid() {
+		var errorItems []go_errors.Item
+
+		for _, resultError := range result.Errors() {
+			errorItems = append(errorItems, go_errors.Item{
+				Message: resultError.Description(),
+				Field:   resultError.Field(),
+			})
+		}
+
+		return go_errors.NewStatusWithItems(http.StatusBadRequest, "Failed schema validation", errorItems)
+	}
+
+	c.logClient.Info(ctx, "Validated")
+	return nil
+}
